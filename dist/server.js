6b2a114a89520c959b28041c63a96dec3f45d3bc
Teepha <teepha06@gmail.com>
2018-11-11 18:31:13 +0100

    push dist to heroku

---
+++ dist/server.js
@@ -0,0 +1,35 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = _interopRequireDefault(require("express"));
+
+var _winston = _interopRequireDefault(require("winston"));
+
+var _routes = _interopRequireDefault(require("./routes"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+// Import router from index.js
+var logger = _winston["default"].createLogger({
+  transports: [new _winston["default"].transports.Console()]
+});
+
+var app = (0, _express["default"])(); // Set port number
+
+var PORT = process.env.PORT || 3030; // Set up Endpoint
+
+app.get('/', function (req, res) {
+  res.send("Welcome to SendIT API!!! <br><br>\n            Available routes: <br>\n            GET  =>  /api/v1/parcels  =>  Fetch all parcel delivery orders <br>\n            GET  =>  /api/v1/parcels/:parcelId  =>  Fetch a specific parcel delivery order <br>\n            GET  =>  /api/v1/users/:userId/parcels  =>  Fetch all parcel delivery orders by a specific user <br>\n            PUT  =>  /api/v1/parcels/:parcelId/cancel  =>  Cancel the specific parcel delivery order <br>\n            POST  =>  /api/v1/parcels  =>  Create a parcel delivery order");
+}); // Mount a middleware function on the path
+
+app.use('/api/v1', _routes["default"]); // Set up the server
+
+app.listen(PORT, function () {
+  return logger.info("Listening on port ".concat(PORT));
+});
+var _default = app;
+exports["default"] = _default;
