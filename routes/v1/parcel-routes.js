c86cdf7fae35f70e8b0aa1fe2f5c6d622aeb0a4f
Teepha <teepha06@gmail.com>
2018-11-15 19:22:18 +0100

    [Bug ##161992578] Verify id parameter

--- routes/v1/parcel-routes.js
+++ routes/v1/parcel-routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import bodyParser from 'body-parser'; // Allows us to receive data sent via POST/PUT API request
 import fs from 'fs';
 import parcelsdb from '../../parcelsdb';
 
-const router = express.Router();
-router.use(bodyParser.json());
+const router = express.Router(); // Create a new instance of express Router
+router.use(bodyParser.json());// Specifically allow us to read data sent in JSON format
 
 // Set up Endpoint to get all parcel orders
 router.get('/parcels', (req, res) => {
@@ -15,21 +15,34 @@ router.get('/parcels', (req, res) => {
 router.get('/parcels/:id', (req, res) => {
   const parcelId = parseInt(req.params.id, 10);
   const foundParcel = parcelsdb.find(parcel => parcel.id === parcelId);
+  if (isNaN(req.params.id)){
+    res.status(400).send('Error! Please enter a number')
+  } else if (req.params.id > parcelsdb[parcelsdb.length-1].id) {
+    res.status(404).send('Error! Parcel not found')
+  } else {
   res.status(200).send(foundParcel);
+  }
 });
 
 // Set up Endpoint to cancel a specific parcel order
 router.put('/parcels/:id/cancel', (req, res) => {
-  const cancelParcel = parcelsdb.find(parcel => parcel.id === parseInt(req.params.id, 10));
-  if (cancelParcel.status === 'Delivered') {
-    res.status(304).send('Parcel Delivered! Cannot cancel parcel order.');
+  const parcelId = parseInt(req.params.id, 10);
+  const parcelToCancel = parcelsdb.find(parcel => parcel.id === parcelId);
+  if (isNaN(req.params.id)){
+    res.status(400).send('Error! Please enter a valid Parcel ID')
+  } else if (!parcelToCancel) {
+    res.status(404).send('Error! Parcel Order Not Found')
+  } else if (parcelToCancel.status === 'Delivered') {
+    res.status(400).send('Parcel Delivered! Cannot cancel Parcel Order.');
+  } else if (parcelToCancel.status === 'Cancelled') {
+    res.status(400).send('Cannot Cancel Parcel Order, Order Already Cancelled.');
   } else {
-    cancelParcel.status = 'Cancelled';
+    parcelToCancel.status = 'Cancelled';
     fs.writeFile('parcelsdb.json', JSON.stringify(parcelsdb, null, 2), (err) => {
       if (err) {
         res.status(500).send(err);
       } else {
-        res.status(202).send(cancelParcel);
+        res.status(202).send(parcelToCancel);
       }
     });
   }
@@ -38,7 +51,7 @@ router.put('/parcels/:id/cancel', (req, res) => {
 // Set up Endpoint to create a new parcel order
 router.post('/parcels', (req, res) => {
   const newParcel = {
-    id: parcelsdb.length + 1,
+    id: parcelsdb[parcelsdb.length-1].id + 1, //find a better way to do this
     userId: req.body.userId,
     pickupLocation: req.body.pickupLocation,
     destination: req.body.destination,
--- routes/v1/user-routes.js
+++ routes/v1/user-routes.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import parcelsdb from '../../parcelsdb';
+import usersdb from '../../usersdb';
+
 
 const router = express.Router();
 
@@ -7,7 +9,13 @@ const router = express.Router();
 router.get('/users/:userId/parcels', (req, res) => {
   const userId = parseInt(req.params.userId, 10);
   const userParcels = parcelsdb.filter(parcel => parcel.userId === userId);
-  res.status(200).send(userParcels);
+  if (isNaN(req.params.userId)){
+    res.status(400).send('Error! Please enter a valid User ID')
+  } else if (userId > usersdb.length) {
+    res.status(404).send('Error! User ID not found')
+  } else {
+    res.status(200).send(userParcels);
+  }
 });
 
 // Export router to index.js
