46dadf3335a48b7e89cebf85273d4b25c641d5f3
Teepha <teepha06@gmail.com>
2018-11-15 19:26:28 +0100

    [Bug #161993135] Create Wildcard route to catch endpoint that does not exist

--- server.js
+++ server.js
@@ -1,29 +1,41 @@
 import express from 'express';
-import winston from 'winston';
+import winston from 'winston'; // for logging
+import path from 'path';
 
 // Import router from index.js
 import v1Routes from './routes';
 
+// set up winston for logging
 const logger = winston.createLogger({
   transports: [
     new winston.transports.Console(),
   ],
 });
 
+// initialize a new express instance
 const app = express();
 
 // Set port number
 const PORT = process.env.PORT || 3030;
 
-// Set up Endpoint
+// Load static assets
+app.use(express.static(path.resolve(__dirname + '/ui')));
+
+// Set up the homepage to serve index.html
 app.get('/', (req, res) => {
-  res.send('Welcome to SendIT API!!!');
+  res.sendFile(path.resolve(__dirname + '/index.html'));
 });
 
 // Mount a middleware function on the path
 app.use('/api/v1', v1Routes);
 
+// Wildcard routes to catch Any kind of request to endpoints that doesn't exist
+app.all('*', (req, res) => {
+  res.status(400).send('Route/Endpoint does not exist!!!')
+});
+
 // Set up the server
 app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
 
+// Export app to test file
 export default app;
