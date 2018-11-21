1e7aac546b8f491b6752ea668626f821019a5967
Teepha <teepha06@gmail.com>
2018-11-21 10:15:17 +0100

    [Feature #162038064] Create endpoint to get all parcel orders by a specific user

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser'; // Allows us to receive data sent via POST/PUT API request
 import { body } from 'express-validator/check'; // to validate request parameter
-import { createUser, getUser } from '../controllers/user-controller';
+import { idChecker, JwtDocoder } from '../middlewares/middleware';
+import { createUser, getUser, getUserParcels } from '../controllers/user-controller';
 
 const router = express.Router(); // Create a new instance of express Router
 router.use(bodyParser.json()); // Specifically allow us to read data sent in JSON format
@@ -13,6 +14,9 @@ router.post('/auth/signup', [
 
 // Login a user
 router.post('/auth/login', body(['email', 'password'], 'Value must be a String').isString(), getUser);
+
+// To get all parcel orders by a specific user
+router.get('/users/:userId/parcels', JwtDocoder, idChecker('userId', ['params'], 'userId should be a number'), getUserParcels);
 
 // Export router to index.js
 export default router;
