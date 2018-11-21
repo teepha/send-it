409ae5457e98765934fe19582df87bed21cda2bb
Teepha <teepha06@gmail.com>
2018-11-21 04:06:53 +0100

    [Feature #162038101] Create user sign in endpoint

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -22,3 +22,23 @@ export const createUser = (req, res) => {
     });
   }
 };
+
+export const getUser = (req, res) => {
+  const {email, password} = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+  } else {
+    const text = 'SELECT * FROM users WHERE email = $1 AND password = $2';
+    const values = [email, password];
+    client.query(text, values, (err, resp) => {       
+        if (err) {
+          res.send(err);
+        } else {
+          const userInfo = resp.rows[0];
+          const token = jwt.sign({ userInfo }, process.env.JWT_SECRET_KEY);
+          res.send({ msg: 'Login successful', token });
+        }
+      });
+  }
+};
