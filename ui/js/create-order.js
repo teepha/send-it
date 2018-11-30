f91097a1abd3977080646e3a26a2a8d158b6f1dc
Teepha <teepha06@gmail.com>
2018-11-30 15:16:29 +0100

    [Feature #162201395] Modify create-order.html file and create create-order.js file

---
+++ ui/js/create-order.js
@@ -0,0 +1,29 @@
+const userId = localStorage.getItem('userId');
+const createOrder = (event) => {
+    event.preventDefault();
+    fetch('/api/v1/parcels', {
+        method: 'POST',
+        body: JSON.stringify({
+            userId,
+            pickupLocation: document.getElementById('pickup_location').value,
+            destination: document.getElementById('destination').value,
+            recipientName: document.getElementById('recipient_name').value,
+            recipientPhone: document.getElementById('recipient_phone').value
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.getItem('token')
+        }
+    }).then(res => res.json())
+        .then(res => {
+            console.log('ress', res);
+            if (res.id){
+                window.location.href = "./user-profile.html";
+            } else {
+                document.querySelector('#error-msg').innerHTML = 'Sorry, incomplete details!';
+            }   
+        }).catch(err => console.log('err occured', err));
+}
+
+const createOrderForm = document.querySelector('#create-order-form');
+createOrderForm.addEventListener('submit', createOrder);
