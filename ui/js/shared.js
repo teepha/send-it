65e46278d973ee1875be9f5693beed69df9b63e1
teepha <teepha06@gmail.com>
2018-12-12 14:54:33 +0100

    [Bug #162196559] Redesign index.html file

--- ui/js/shared.js
+++ ui/js/shared.js
@@ -4,3 +4,21 @@ document.querySelectorAll('#log_out').forEach(item => {
         localStorage.clear();
     })
 });
+
+
+// Nav bar for Media queries @max-width: 520px
+const navbarBtn = document.querySelector('.navbar__btn')
+const navbarLinks = document.querySelector('.navigation')
+
+
+navbarBtn.addEventListener('click', function(){
+    let value = navbarLinks.classList.contains('navbar__collapse')
+
+    if(value){
+        navbarLinks.classList.remove('navbar__collapse')
+        navbarBtn.classList.remove('change')
+    }else{
+        navbarLinks.classList.add('navbar__collapse')
+        navbarBtn.classList.add('change')
+    }
+})
