8332b0a58a9712ab003649209bde1ad58ce3a5ba
Lateefat Amuda <teepha06@gmail.com>
2019-03-13 08:53:20 +0100

    add acam methods

--- server/server.js
+++ server/server.js
@@ -79,6 +79,7 @@ const PORT = process.env.PORT || 3030;
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, PATCH, DELETE, OPTIONS');
   next();
 });
 
